d1233bc47a5315c2dd75cf43b4033954a9a561f6
Toivi <60314853+casualGamer-dev@users.noreply.github.com>
2024-05-22 19:07:36 +0530

    Update gogoanime.ts

--- anify-backend/src/mappings/impl/anime/gogoanime.ts
+++ anify-backend/src/mappings/impl/anime/gogoanime.ts
@@ -80,7 +80,7 @@ export default class GogoAnime extends AnimeProvider {
                 title: $(el).find(`div.name`).text(),
                 isFiller: false,
                 img: null,
-                hasDub: $(el).find('p.name > a').text().toLowerCase().includes('dub'),
+                hasDub: id.includes("-dub"),
                 description: null,
                 rating: null,
             });
